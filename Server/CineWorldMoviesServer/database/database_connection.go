752b6eb71b1220317f0c599f377034537bcb9963
Jack Aakash <jackaakash4@gmail.com>
2026-02-09 23:01:51 +0545

    Connected to mongodb

--- Server/CineWorldMoviesServer/database/database_connection.go
+++ Server/CineWorldMoviesServer/database/database_connection.go
@@ -1,12 +1,35 @@
 package database
 
-import(
+import (
 	"fmt"
 	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
-func 
+func DBInstance() *mongo.Client {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Println("Warning: Unable to find .env file")
+	}
+
+	MongoDb := os.Getenv("MONGODB_URI")
+	if MongoDb == "" {
+		log.Fatal("Warning: Unable to find mongodb uri")
+	}
+
+	fmt.Println("MongoDB URI: ", MongoDb)
+
+	clientOptions := options.Client().ApplyURI(MongoDb)
+
+	client, err := mongo.Connect(clientOptions)
+	if err != nil {
+		return nil
+	}
+
+	return client
+}
